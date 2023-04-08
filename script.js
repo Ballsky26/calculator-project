e4867091cfa9e7855093697cd6b9bc044ed41500
Ballsky22 <ballskyteam22@gmail.com>
2023-04-08 13:39:25 +0700

    Math Operation Javascript

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const displayHistory = document.querySelector(".display-history");
 const display = document.querySelector(".display-input");
 const tempResult = document.querySelector(".temp-result");
-const number = document.querySelectorAll(".number");
-const operation = document.querySelectorAll(".operation");
+const numbers = document.querySelectorAll(".number");
+const operations = document.querySelectorAll(".operation");
 const equal = document.querySelector(".equal");
 const clearAll = document.querySelector(".all-clear");
 const clearLast = document.querySelector(".last-entity-clear");
@@ -13,7 +13,7 @@ let result = null;
 let lastOperation = "";
 let haveDot = false;
 
-number.forEach((number) => {
+numbers.forEach((number) => {
   number.addEventListener("click", (e) => {
     if (e.target.innerText === "." && !haveDot) {
       haveDot = true;
@@ -24,3 +24,40 @@ number.forEach((number) => {
     display.innerText = dis2Num;
   });
 });
+
+operations.forEach((operation) => {
+  operation.addEventListener("click", (e) => {
+    if (!dis2Num) return;
+    haveDot = false;
+    const operationName = e.target.innerText;
+    if (dis1Num && dis2Num && lastOperation) {
+      mathOperation();
+    } else {
+      result = parseFloat(dis2Num);
+    }
+    clearVar(operationName);
+    lastOperation = operationName;
+  });
+});
+
+function clearVar(name = "") {
+  dis1Num += dis2Num + " " + name + " ";
+  displayHistory.innerText = dis1Num;
+  display.innerText = "";
+  dis2Num = "";
+  tempResult.innerText = result;
+}
+
+function mathOperation() {
+  if (lastOperation === "X") {
+    result = parseFloat(result) * parseFloat(dis2Num);
+  } else if (lastOperation === "+") {
+    result = parseFloat(result) + parseFloat(dis2Num);
+  } else if (lastOperation === "-") {
+    result = parseFloat(result) - parseFloat(dis2Num);
+  } else if (lastOperation === "/") {
+    result = parseFloat(result) / parseFloat(dis2Num);
+  } else if (lastOperation === "&") {
+    result = parseFloat(result) + parseFloat(dis2Num);
+  }
+}
