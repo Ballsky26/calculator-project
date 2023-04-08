e990cdd54bcb398713c1e14be05abaf1a4337431
Ballsky22 <ballskyteam22@gmail.com>
2023-04-08 14:05:48 +0700

    Key Window Operation

--- script.js
+++ script.js
@@ -88,3 +88,52 @@ clearLast.addEventListener("click", () => {
   display.innerText = "";
   dis2Num = "";
 });
+
+window.addEventListener("keydown", (e) => {
+  if (
+    e.key === "0" ||
+    e.key === "1" ||
+    e.key === "2" ||
+    e.key === "3" ||
+    e.key === "4" ||
+    e.key === "5" ||
+    e.key === "6" ||
+    e.key === "7" ||
+    e.key === "8" ||
+    e.key === "9"
+  ) {
+    clickButton(e.key);
+  } else if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "%") {
+    clickOperation(e.key);
+  } else if (e.key === "*") {
+    clickOperation("X");
+  } else if (e.key === "Enter" || e.key === "=") {
+    clickEqual();
+  } else if (e.key === "Backspace") {
+    clickClear();
+  }
+});
+
+function clickButton(key) {
+  numbers.forEach((button) => {
+    if (button.innerText === key) {
+      button.click();
+    }
+  });
+}
+
+function clickOperation(key) {
+  operations.forEach((operation) => {
+    if (operation.innerText === key) {
+      operation.click();
+    }
+  });
+}
+
+function clickEqual() {
+  equal.click();
+}
+
+function clickClear() {
+  clearAll.click();
+}
